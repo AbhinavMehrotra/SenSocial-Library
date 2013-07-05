ca0c01cccb85dcce412b7d99afb2de310f211a90
Abhinav Mehrotra <abhinav.iims@gmail.com>
2013-07-05 23:10:10 +0100

    Added a test file

---
+++ SenSocialServer/src/com/ubhave/sensocial/server/manager/RunTest.java
@@ -0,0 +1,13 @@
+package com.ubhave.sensocial.server.manager;
+
+public class RunTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("It Works Fine!!");
+	}
+
+}
