e9cef3fa056ba8b240430787b9c657865d7ce99d
axm514 <axm514@bham.ac.uk>
2014-05-09 18:41:27 +0100

    Copyright added

--- SenSocialServer/src/com/ubhave/sensocial/server/tcp/MessageParser.java
+++ SenSocialServer/src/com/ubhave/sensocial/server/tcp/MessageParser.java
@@ -1,3 +1,35 @@
+/*******************************************************************************
+ *
+ * SenSocial Middleware
+ *
+ * Copyright (c) ${2014}, University of Birmingham
+ * Abhinav Mehrotra, axm514@cs.bham.ac.uk
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Birmingham 
+ *       nor the names of its contributors may be used to endorse or
+ *       promote products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************/
 package com.ubhave.sensocial.server.tcp;
 
 import org.json.JSONException;
@@ -66,11 +98,11 @@ public class MessageParser {
 	
 	
 /*
-		Registration:  { “name”: “registration”, "userid":<...>, “deviceid”:”<<unique_device_id>>”, "bluetoothmac":"<...>"}
-		OSN authentication:  { “name”: “osn”, "userid":<...>,  “deviceid”:”<<unique_device_id>>”, “osn”:”<<json_object_for_specific_osn>>” }
-		json_object_for_specific_osn: {“osnname”:””, ”name”:””,  ”userid”:””, “username”:””, “token”:”” }
-		Location tracking:  { “name”: “location”, "userid":<...>,  “deviceid”:”<<unique_device_id>>”, “location”:”{“lat”:0,”lon”:0}” }
-		Stream:  { “name”: “stream”, "userid":<...>,  “deviceid”:”<<unique_device_id>>”, “stream”:”social_event”}
+		Registration:  { ï¿½nameï¿½: ï¿½registrationï¿½, "userid":<...>, ï¿½deviceidï¿½:ï¿½<<unique_device_id>>ï¿½, "bluetoothmac":"<...>"}
+		OSN authentication:  { ï¿½nameï¿½: ï¿½osnï¿½, "userid":<...>,  ï¿½deviceidï¿½:ï¿½<<unique_device_id>>ï¿½, ï¿½osnï¿½:ï¿½<<json_object_for_specific_osn>>ï¿½ }
+		json_object_for_specific_osn: {ï¿½osnnameï¿½:ï¿½ï¿½, ï¿½nameï¿½:ï¿½ï¿½,  ï¿½useridï¿½:ï¿½ï¿½, ï¿½usernameï¿½:ï¿½ï¿½, ï¿½tokenï¿½:ï¿½ï¿½ }
+		Location tracking:  { ï¿½nameï¿½: ï¿½locationï¿½, "userid":<...>,  ï¿½deviceidï¿½:ï¿½<<unique_device_id>>ï¿½, ï¿½locationï¿½:ï¿½{ï¿½latï¿½:0,ï¿½lonï¿½:0}ï¿½ }
+		Stream:  { ï¿½nameï¿½: ï¿½streamï¿½, "userid":<...>,  ï¿½deviceidï¿½:ï¿½<<unique_device_id>>ï¿½, ï¿½streamï¿½:ï¿½social_eventï¿½}
 */
 	
 }
